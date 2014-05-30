2ccc28f01c7ff3c3e10d3ff49de6560a32e4ff89
john <john@hookscrane.com>
2014-05-30 06:03:59 -0700

    added racket to .path

--- unpack.sh
+++ unpack.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
-# Lots barrowed from github.com/mathiasbyens/dotfiles/bootstrap.sh
+# Lots borrowed from github.com/mathiasbyens/dotfiles/bootstrap.sh
 
 git pull origin master
+
 function unpack() {
 	rsync --exclude ".git/" --exclude "unpack.sh" --exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" --exclude "disable*" --exclude "boot*" -av --no-perms . ~
+	  --exclude "LICENSE-MIT.txt" --exclude "disable*" -av --no-perms . ~
 	source ~/.bash_profile
 }
+
 read -p "This may overwrite existing files in your home directory. Continue?[y/n] " -n 1
+
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 	unpack
 fi
+
 unset unpack
