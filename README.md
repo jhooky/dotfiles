2e5a63c28d85feca10b39ed524f5d9201a4d35f9
john <john@hookscrane.com>
2014-12-16 16:52:38 -0800

    Improve 'README.md' and clean up 'mash.sh'

--- README.md
+++ README.md
@@ -1,20 +1,28 @@
 # lookyhooky/dotfiles
-```bash
-                        __    
-   ///\\ ///||| ///||///  \\\||  |||
-  ///  \\// |||/// ||\\\__  |||  |||
- ///    //  ||///==|||    \\\||==|||
-///         |///   ||\\\__///||  |||.sh
 
 ```
-## purpose
+      __            __         __                __
+     / /___  ____  / /____  __/ /_  ____  ____  / /____  __
+    / / __ \/ __ \/ //_/ / / / __ \/ __ \/ __ \/ //_/ / / /
+   / / /_/ / /_/ / ,< / /_/ / / / / /_/ / /_/ / ,< / /_/ /
+  /_/\____/\____/_/|_|\__, /_/ /_/\____/\____/_/|_|\__, /
+                    /____/                       /____/
+
+```
+
+## Purpose
 
 To create a portable set of configuration files that can mash well with others.
 
-## installation
+## Installation
 
-mash.sh will link all dotfiles to home directory, if there are any conflicts there will be chances to resolve them.
+Clone repo and source `mash.sh` it will link all dotfiles to your home directory. If there is conflict with previous files, there will be chance to remove and link or skip that file.
 
 ```bash
 git clone https://github.com/lookyhooky/dotfiles.git .dotfiles && cd .dotfiles && source mash.sh
 ```
+
+## ToDo
+
+* I believe that `ls` has a different set of commands in linux. I need to compare to OSX, on which this was built.
+* I want to break out most of the functions of mash.sh into a separate file to use in different projects.
--- mash.sh
+++ mash.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
 # mash.sh by lookyhooky
-#                        __    
-#   ///\\ ///||| ///||///  \\\||  |||
-#  ///  \\// |||/// ||\\\__  |||  |||
-# ///    //  ||///==|||    \\\||==|||
-#///         |///   ||\\\__///||  |||.sh
+#     __            __         __                __
+#    / /___  ____  / /____  __/ /_  ____  ____  / /____  __
+#   / / __ \/ __ \/ //_/ / / / __ \/ __ \/ __ \/ //_/ / / /
+#  / / /_/ / /_/ / ,< / /_/ / / / / /_/ / /_/ / ,< / /_/ /
+# /_/\____/\____/_/|_|\__, /_/ /_/\____/\____/_/|_|\__, /
+#                    /____/                       /____/
 
 set -e
-
-RCol='\\033[0m'    # Text Reset
-
-# Regular      
-Bla='\\033[0;30m';
-Red='\\033[0;31m';
-Gre='\\033[0;32m';
-Yel='\\033[0;33m';
-Blu='\\033[0;34m';
-Pur='\\033[0;35m';
-Cya='\\033[0;36m';
-Whi='\\033[0;37m';
 
 SRC_DIR="$(pwd)/src"    # source directory
 DST_DIR="$HOME"         # destination directory
@@ -32,17 +21,17 @@ colortext() {
 
 error_exit () {
   # expecrs $1 as a string representing an error message
-  
+
   echo "$1" 1>&2
   exit 1
-  
+
 }
 
 send_message () {
-  # expects $1 as flash $2 as ansi color code and $3 as message
-  
+  # expects $1 as flash $2 as ancii color code and $3 as message
+
   echo -e "[$(colortext $1 $2)] $3"
-  
+
 }
 
 compare_link () {
@@ -55,20 +44,20 @@ compare_link () {
   else
     return 1
   fi
-  
+
 }
 
 link_file () {
   # expects $1 as source file and $2 as destination file
-  
+
   ln -s $1 $2
-  send_message "Success" "32" "$2 linked to source"
-  
+  send_message "Mashed!" "32" "$2 linked to source"
+
 }
 
 prompt_removal () {
   # expects $1 as a path to the file to remove
-  
+
   while true; do
     read -p "Do you wish to remove $1? " yn
 
@@ -83,7 +72,7 @@ prompt_removal () {
       *)
         send_message "FAILURE" "31" "Please answer 'yes' or 'no'";;
     esac
-    
+
   done
 
 }
@@ -93,14 +82,15 @@ cat << "EOF"
 
 Welcome to lookyhooky/dotfiles... may they mash well with yours
 
-                         __    
-    ///\\ ///||| ///||///  \\\||  |||
-   ///  \\// |||/// ||\\\__  |||  |||
-  ///    //  ||///==|||    \\\||==|||
- ///         |///   ||\\\__///||  |||.sh
-                                               
+                            __
+   ///////\\ ///||| ///||///  \\\||  |||
+  ///////  \\// |||/// ||\\\__  |||  |||
+ ///////    //  ||///==|||    \\\||==|||
+///////         |///   ||\\\__///||  |||
+
+Symlinking dotfiles...
+
 EOF
-echo "Symlinking dotfiles... "
 
 src_files="$(ls -1F $SRC_DIR)"
 
@@ -108,23 +98,23 @@ for file in $src_files ; do
 
   src_path="$SRC_DIR/$file"
   dst_path="$DST_DIR/.$file"
-  
+
   if [ -f $dst_path ] ; then
 
     if [ -h $dst_path ] && compare_link $DST_DIR $src_path ; then
       # there is already a link to the source file, roll on
       send_message "Rolling" "36" "$dst_path links to source file"
-      
+
     elif [ -h $dst_path ] ; then
       # there is a unknown link in place of where the link should go
       send_message "Warning" "33" "$dst_path links to unknown file"
-      
+
       if prompt_removal $dst_path ; then
         link_file $src_path $dst_path
       else
         send_message "FAILURE" "31" "$dst_path not linked to source file"
       fi
-       
+
     else
       # there is a regular file in place of where the link should go
       send_message "Warning" "33" "$dst_path is an unknown regular file"
@@ -136,14 +126,14 @@ for file in $src_files ; do
       fi
 
     fi
-   
+
   else
     # there is nothing in the way of linking the file
-    
+
     link_file $src_path $dst_path
-    
-  fi 
-    
+
+  fi
+
 done
 
 cat <<EOF
@@ -151,5 +141,3 @@ cat <<EOF
 Boy! That was painless!
 
 EOF
-# list_of_files_in_home="$(ls -1AF $DST_DIR | grep -Ev '/$' )"
-
